a10f7f4e436faa9fde8b24703259b492562f2b30
Mateus Melo <mateusdevmelo@gmail.com>
2024-01-29 21:30:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var encryptButton = document.querySelector('.btn-encriptar');
+var decryptButton = document.querySelector('.btn-desencriptar');
+var contentBoy = document.querySelector('.content-boy');
+var contentParagraph = document.querySelector('.content-paragraph');
+var result = document.querySelector('.content-result');
+
+encryptButton.onclick = encriptar;
+decryptButton.onclick = desencriptar;
+
+function encriptar() {
+    
+}
