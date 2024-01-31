913d2136665540831cb1bfe323b3e019db02da57
Mateus Melo <mateusdevmelo@gmail.com>
2024-01-30 21:21:04 -0300

    "second commit"

--- script.js
+++ script.js
@@ -8,5 +8,87 @@ encryptButton.onclick = encriptar;
 decryptButton.onclick = desencriptar;
 
 function encriptar() {
-    
+   ocultarPalavra();
+   var caixaTexto = recuperarTexto()
+   result.textContent = encriptarTexto(caixaTexto); 
+}
+
+function desencriptar() {
+    ocultarPalavra();
+    var caixaTexto = recuperarTexto()
+    result.textContent = desencriptarTexto(caixaTexto);
+}
+
+function recuperarTexto() {
+    var caixaTexto = document.querySelector(".text-box-two")
+    return caixaTexto.value
+}
+
+function ocultarPalavra() {
+    contentBoy.classList.add("ocultar");
+    contentParagraph.classList.add("ocultar");
+}
+
+function encriptarTexto(mensagem) {
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++) {
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+
+}
+
+function desencriptarTexto(mensagem){
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i];
+        }
+        
+    }
+
+    return textoFinal;
+
 }
